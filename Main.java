5f087ed999a478b9c8bba450fb8d8ae34d36266d
redu-leul <leulrediet@gmail.com>
2026-04-26 20:43:37 +0300

    Implement Main class with car object creation

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void Main(String[] args) {
+        car myCar = new car("Toyota", 2020, "Corolla");
+        myCar.honk();  
+        myCar.display();
+        
+    }
+}
